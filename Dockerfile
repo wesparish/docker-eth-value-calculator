e68854556072dedbebe022b930b34ad4edf15aea
wes <wes@home.local>
2018-01-01 14:20:58 +0000

    CSS looks ok

--- Dockerfile
+++ Dockerfile
@@ -2,15 +2,11 @@ FROM ubuntu:16.04
 
 RUN apt-get update && \
     apt-get install python3-pip git python3 vim-tiny curl -y && \
-    pip3 install requests && \
-    git clone https://github.com/corpetty/py-etherscan-api.git /py-etherscan-api && \
-    cd /py-etherscan-api && \
-    python3 setup.py install && \
-    cd / && \
+    pip3 install flask requests && \
     apt-get autoremove -y && \
     apt-get clean
 
-ENV LOG_LEVEL="WARN" 
+ENV LOG_LEVEL="WARN" LC_ALL=C.UTF-8 LANG=C.UTF-8
 
 COPY eth-value-calculator.py /eth-value-calculator.py
 RUN chown root:root eth-value-calculator.py
--- eth-value-calculator.py
+++ eth-value-calculator.py
@@ -5,13 +5,22 @@ import requests
 import csv
 import sys
 from optparse import OptionParser
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return render_template('index.html')
 
 parser = OptionParser()
 parser.add_option("-a", "--address", dest="address",
-                  help="ETH address", metavar="ADDRESS")
+                  help="ETH address")
+parser.add_option("-d", "--debug", dest="debug", default=False,
+                  help="Debug mode", action="store_true")
+parser.add_option("-s", "--server", dest="server", default=False,
+                  help="Run web server", action="store_true")
 (options, args) = parser.parse_args()
-
-address = options.address
 
 def get_eth_price(timestamp):
   url = "https://min-api.cryptocompare.com/data/pricehistorical?fsym=ETH&tsyms=USD&ts=%s" % (timestamp)
@@ -25,21 +34,27 @@ def get_etherscan_transactions(address):
   r.raise_for_status()
   return r.json()['result']
 
-transaction_list = get_etherscan_transactions(address)
+def get_all_transactions(address):
+  transaction_list = get_etherscan_transactions(address)
+  
+  csv_writer = csv.writer(sys.stdout)
+  csv_writer.writerow([ "Block Number",
+                        "Timestamp",
+                        "Value ETH",
+                        "Value USD" ])
+  for transaction in transaction_list:
+    # Filter for nanopool and ethermine, respectively
+    if (transaction['from'] == '0x52bc44d5378309ee2abf1539bf71de1b7d7be3b5') or \
+       (transaction['from'] == '0xEA674fdDe714fd979de3EdF0F56AA9716B898ec8'):
+      value_eth = float(transaction['value']) / 1000000000000000000.0
+      value_usd = value_eth * get_eth_price(transaction['timeStamp'])
+      csv_writer.writerow([ transaction['blockNumber'],
+                            transaction['timeStamp'],
+                            value_eth,
+                            value_usd ])
 
-csv_writer = csv.writer(sys.stdout)
-csv_writer.writerow([ "Block Number",
-                      "Timestamp",
-                      "Value ETH",
-                      "Value USD" ])
-for transaction in transaction_list:
-  # Filter for nanopool and ethermine, respectively
-  if (transaction['from'] == '0x52bc44d5378309ee2abf1539bf71de1b7d7be3b5') or \
-     (transaction['from'] == '0xEA674fdDe714fd979de3EdF0F56AA9716B898ec8'):
-    value_eth = float(transaction['value']) / 1000000000000000000.0
-    value_usd = value_eth * get_eth_price(transaction['timeStamp'])
-    csv_writer.writerow([ transaction['blockNumber'],
-                          transaction['timeStamp'],
-                          value_eth,
-                          value_usd ])
+if options.server:
+  app.run(debug=options.debug, host='0.0.0.0')
+else:
+  get_all_transactions(options.address)
 
